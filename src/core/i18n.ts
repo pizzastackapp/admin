eff1a7542a9c14ca52014b26b95fbc9c727e7f29
Dmytro Batarin <dmytro@batarin.dev>
2022-10-17 21:12:13 +0300

    Hot fix for translatio

--- src/core/i18n.ts
+++ src/core/i18n.ts
@@ -1,4 +1,14 @@
 import polyglotI18nProvider from 'ra-i18n-polyglot';
 import ua from 'ra-language-ukrainian';
 
-export const i18nProvider = polyglotI18nProvider(() => ua, 'ua');
+const lang = {
+  ra: {
+    ...ua.ra,
+    configurable: {
+      ...ua.ra.configurable,
+      customize: 'customize',
+    },
+  },
+};
+
+export const i18nProvider = polyglotI18nProvider(() => lang, 'ua');
